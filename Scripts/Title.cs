b655c030b0f35dd5bfe8dbf19eac1c50ff73347f
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2023-10-09 15:02:59 +0300

    Added generic title scene

---
+++ Scripts/Title.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+[Tool]
+public class Title : Node
+{
+    [Export]
+    public string Text
+    {
+        get => label.Text;
+        set => label.Text = value;
+    }
+    [Export]
+    private NodePath pathLabel;
+    private Label _label = null;
+    private Label label => _label ?? (_label = GetNode<Label>(pathLabel));
+}
